ceb41a3d50e81d42fd358ce49e8799959c288388
Robert Van Voorhees <rcvanvo@gmail.com>
2016-04-19 08:37:24 -0400

    Adding some DTO samples.

---
+++ src/main/java/net/smartcosmos/dto/objects/CreateObjectDTO.java
@@ -0,0 +1,25 @@
+package net.smartcosmos.dto.objects;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author voor
+ */
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreateObjectDTO {
+    @NotNull
+    private String objectUrn;
+    @NotNull
+    private String type;
+    @NotNull
+    private String name;
+    private String description;
+    private Boolean activeFlag = true;
+    private String moniker;
+    private String returnValueType;
+}
---
+++ src/main/java/net/smartcosmos/dto/objects/GetObjectDTO.java
@@ -0,0 +1,28 @@
+package net.smartcosmos.dto.objects;
+
+import lombok.Builder;
+import lombok.Data;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * @author voor
+ */
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class GetObjectDTO {
+    private final String urn;
+
+    private final String objectUrn;
+
+    private final String type;
+
+    private final String accountUrn;
+    private final String name;
+    private Long lastModifiedTimestamp;
+    private String moniker;
+    private String description;
+
+    private Boolean activeFlag;
+}
