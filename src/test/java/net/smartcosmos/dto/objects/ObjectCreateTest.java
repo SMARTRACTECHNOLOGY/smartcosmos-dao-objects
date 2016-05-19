934fecfcf8d138733a99b5af8c2d3eafcbf7fe3d
Robert Van Voorhees <rcvanvo@gmail.com>
2016-05-19 11:15:31 -0400

    Adds in a very simple unit test example.

---
+++ src/test/java/net/smartcosmos/dto/objects/ObjectCreateTest.java
@@ -0,0 +1,18 @@
+package net.smartcosmos.dto.objects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Basic Unit Tests for the ObjectCreate object...
+ */
+public class ObjectCreateTest {
+    @Test
+    public void thatObjectCreateAlwaysHasActiveFlag() throws Exception {
+        ObjectCreate objectCreate = ObjectCreate.builder().objectUrn("objectUrn").type("type").name("name").build();
+
+        assertTrue(objectCreate.getActiveFlag());
+    }
+
+}
