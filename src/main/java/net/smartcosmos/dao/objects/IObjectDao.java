570ed3fcfc8dfba9bf647bfadebfda19ec66afe1
Will Schneider <will.schneider@smartrac-group.com>
2016-05-20 14:42:19 -0400

    add enum to IObjectDao for search query params

--- src/main/java/net/smartcosmos/dao/objects/IObjectDao.java
+++ src/main/java/net/smartcosmos/dao/objects/IObjectDao.java
@@ -1,13 +1,13 @@
 package net.smartcosmos.dao.objects;
 
-import net.smartcosmos.dto.objects.ObjectCreate;
-import net.smartcosmos.dto.objects.ObjectResponse;
-import net.smartcosmos.dto.objects.ObjectUpdate;
-
-import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import javax.validation.ConstraintViolationException;
+
+import net.smartcosmos.dto.objects.ObjectCreate;
+import net.smartcosmos.dto.objects.ObjectResponse;
+import net.smartcosmos.dto.objects.ObjectUpdate;
 
 /**
  * @author voor
@@ -24,6 +24,46 @@ public interface IObjectDao {
 
     List<ObjectResponse> getObjects();
 
-    List<ObjectResponse> findByQueryParameters(String accountUrn, Map<String, Object> parameters);
+    List<ObjectResponse> findByQueryParameters(String accountUrn, Map<QueryParameterType, Object> parameters);
 
+    /**
+     * An Enum defining the query parameter type names when searching for Things in Objects.
+     */
+    public static enum QueryParameterType {
+        /**
+         * Search for Things with by urn.
+         */
+        OBJECT_URN_LIKE("objectUrnLike"),
+        /**
+         * Search for things by type.
+         */
+        TYPE("type"),
+        /**
+         * Search for things by name.
+         */
+        NAME_LIKE("nameLike"),
+        /**
+         * Search for things by moniker.
+         */
+        MONIKER_LIKE("monikerLike"),
+        /**
+         * Search for things by the timestamp it was last modified.
+         */
+        MODIFIED_AFTER("modifiedAfter");
+
+        private String parameterName;
+
+        private QueryParameterType(String name) {
+            parameterName = name;
+        }
+
+        /**
+         * The name of the parameter to be used in the query.
+         *
+         * @return the parameter type name as a String
+         */
+        public String typeName() {
+            return parameterName;
+        }
+    }
 }
