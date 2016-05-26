aa0554a94032a9df90893da624afae80948c693e
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-05-26 13:47:07 +0200

    Remove constraint annotation code, validator and tests

--- src/test/java/net/smartcosmos/dto/objects/ObjectCreateTest.java
+++ src/test/java/net/smartcosmos/dto/objects/ObjectCreateTest.java
@@ -1,16 +1,7 @@
 package net.smartcosmos.dto.objects;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-import java.util.Set;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -19,50 +10,10 @@ import static org.junit.Assert.assertTrue;
 @SuppressWarnings("Duplicates")
 public class ObjectCreateTest {
 
-    private static Validator validator;
-
-    @BeforeClass
-    public static void setUp() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
-    }
-
     @Test
     public void thatObjectCreateAlwaysHasActiveFlag() throws Exception {
         ObjectCreate objectCreate = ObjectCreate.builder().objectUrn("objectUrn").type("type").name("name").build();
 
         assertTrue(objectCreate.getActiveFlag());
     }
-
-    @Test
-    public void thatObjectCreateMustHaveObjectURN() {
-
-        ObjectCreate objectCreate = ObjectCreate.builder()
-            .type("type")
-            .name("name").build();
-
-        Set<ConstraintViolation<ObjectCreate>> constraintViolations = validator.validate(objectCreate);
-
-        assertFalse(constraintViolations.isEmpty());
-
-        assertEquals(1, constraintViolations.size() );
-        assertEquals("may not be empty", constraintViolations.iterator().next().getMessage());
-    }
-
-    @Test
-    public void thatObjectCreateMustNotHaveEmptyObjectURN() {
-
-        ObjectCreate objectCreate = ObjectCreate.builder()
-            .objectUrn("")
-            .type("type")
-            .name("name").build();
-
-        Set<ConstraintViolation<ObjectCreate>> constraintViolations = validator.validate(objectCreate);
-
-        assertFalse(constraintViolations.isEmpty());
-
-        assertEquals(1, constraintViolations.size() );
-        assertEquals("may not be empty", constraintViolations.iterator().next().getMessage());
-    }
-
 }
