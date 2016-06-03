2a9a278de2e69e4f04688214eda8fd8f6471191d
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-03 11:03:05 +0200

    Add version to DTOs

--- src/main/java/net/smartcosmos/dto/objects/ObjectResponse.java
+++ src/main/java/net/smartcosmos/dto/objects/ObjectResponse.java
@@ -1,17 +1,21 @@
 package net.smartcosmos.dto.objects;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Setter;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import java.beans.ConstructorProperties;
 
-/**
- * @author voor
- */
 @Data
-@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ObjectResponse {
+
+    private static final int VERSION = 1;
+    @Setter(AccessLevel.NONE)
+    private int version = VERSION;
+
     private final String urn;
 
     private final String objectUrn;
@@ -25,4 +29,20 @@ public class ObjectResponse {
     private String description;
 
     private Boolean activeFlag;
+
+    @Builder
+    @ConstructorProperties({"urn", "objectUrn", "type", "accountUrn", "name", "lastModifiedTimestamp", "moniker", "description", "activeFlag"})
+    public ObjectResponse(String urn, String objectUrn, String type, String accountUrn, String name, Long lastModifiedTimestamp, String moniker, String description, Boolean activeFlag) {
+        this.urn = urn;
+        this.objectUrn = objectUrn;
+        this.type = type;
+        this.accountUrn = accountUrn;
+        this.name = name;
+        this.lastModifiedTimestamp = lastModifiedTimestamp;
+        this.moniker = moniker;
+        this.description = description;
+        this.activeFlag = activeFlag;
+
+        this.version = VERSION;
+    }
 }
