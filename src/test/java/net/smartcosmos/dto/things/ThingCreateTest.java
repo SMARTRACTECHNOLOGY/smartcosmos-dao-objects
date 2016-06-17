6406bc95b4c07b134adcbfce5031515992773745
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-17 10:09:21 +0200

    Replace star import

--- src/test/java/net/smartcosmos/dto/things/ThingCreateTest.java
+++ src/test/java/net/smartcosmos/dto/things/ThingCreateTest.java
@@ -2,7 +2,10 @@ package net.smartcosmos.dto.things;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Basic Unit Tests for the ThingCreate object...
--- src/test/java/net/smartcosmos/dto/things/ThingResponseTest.java
+++ src/test/java/net/smartcosmos/dto/things/ThingResponseTest.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 public class ThingResponseTest {
