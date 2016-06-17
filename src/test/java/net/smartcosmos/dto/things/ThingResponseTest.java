0d9d4e5ace8b8778afed789a604e720121c32ee3
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-17 09:49:57 +0200

    Remove id in ThingResponse, rename tenantId to tenantUrn

--- src/test/java/net/smartcosmos/dto/things/ThingResponseTest.java
+++ src/test/java/net/smartcosmos/dto/things/ThingResponseTest.java
@@ -5,13 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.junit.Test;
 
-import java.lang.reflect.Method;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 
 public class ThingResponseTest {
@@ -27,15 +21,9 @@ public class ThingResponseTest {
     /**
      * This actually tests if Lombok is properly used.
      */
-    @Test
-    public void thatVersionHasNoSetter() {
-        Method getVersion = null;
-        try {
-            getVersion = ThingResponse.class.getDeclaredMethod("setVersion", int.class);
-        } catch (NoSuchMethodException e) {
-            // that's what we expect
-        }
-        assertNull(getVersion);
+    @Test(expected = NoSuchMethodException.class)
+    public void thatVersionHasNoSetter() throws Exception {
+        ThingResponse.class.getDeclaredMethod("setVersion", int.class);
     }
 
     @Test
