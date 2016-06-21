e34a1a6d5dde24dd2b7b6b6fdc0479c7c4c5dd37
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-21 08:31:11 +0200

    Add builder for Page

--- src/main/java/net/smartcosmos/dto/things/Page.java
+++ src/main/java/net/smartcosmos/dto/things/Page.java
@@ -3,9 +3,12 @@ package net.smartcosmos.dto.things;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
+import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
+import java.beans.ConstructorProperties;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -13,6 +16,17 @@ import java.util.List;
 @ApiModel(description = "Paged response received when querying for Things.")
 public class Page<T> {
 
-    private final List<T> data;
+    private final Collection<T> data;
     private final PageInformation page;
+
+    @Builder
+    @ConstructorProperties({"data", "page"})
+    public Page(Collection<T> data, PageInformation page) {
+        this.data = new ArrayList<>();
+        if (data != null) {
+            this.data.addAll(data);
+        }
+
+        this.page = page;
+    }
 }
