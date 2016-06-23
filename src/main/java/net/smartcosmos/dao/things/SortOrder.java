223aa75db4b33c8460e50fb8742bcd7bbead6444
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-23 11:50:09 +0200

    Add method to get sort order from String

--- src/main/java/net/smartcosmos/dao/things/SortOrder.java
+++ src/main/java/net/smartcosmos/dao/things/SortOrder.java
@@ -1,6 +1,25 @@
 package net.smartcosmos.dao.things;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum SortOrder {
-    ASC,
-    DESC
+    ASC("asc"),
+    DESC("desc");
+
+    private final String sortOrder;
+
+    @Override
+    public String toString() {
+        return this.sortOrder;
+    }
+
+    public static SortOrder fromString(String string) {
+        String lcString = "";
+        if (string != null) {
+            lcString = string.toLowerCase();
+        }
+
+        return (DESC.toString().equals(lcString) ? DESC : ASC);
+    }
 }
