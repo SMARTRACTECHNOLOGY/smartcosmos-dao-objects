8a5c219868898c9e9ae83c70a9aa5c349cc535e1
Will Schneider <will.schneider@smartrac-group.com>
2016-06-24 14:45:22 -0400

    remove swagger annotations from DAO class that should never go over the wire

--- src/main/java/net/smartcosmos/dto/things/ThingResponse.java
+++ src/main/java/net/smartcosmos/dto/things/ThingResponse.java
@@ -1,38 +1,25 @@
 package net.smartcosmos.dto.things;
 
+import java.beans.ConstructorProperties;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
 
-import java.beans.ConstructorProperties;
-
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties({"version"})
-@ApiModel(description = "Response received when querying for Things.")
 public class ThingResponse {
 
     private static final int VERSION = 1;
     @Setter(AccessLevel.NONE)
     private int version = VERSION;
-
-    @ApiModelProperty(required = true, dataType = "String",
-                      value = "The URN of the thing. The size and format are constrained by the database implementation being used.")
     private final String urn;
-
-    @ApiModelProperty(required = true, dataType = "String",
-                      value = "The user defined type of the thing.  The size is constrained by the database implementation being used.")
     private final String type;
-
-    @ApiModelProperty(required = true, dataType = "String", value = "The tenant URN of the owning account of the thing.")
     private final String tenantUrn;
-
-    @ApiModelProperty(required = true, dataType = "Boolean", value = "Is the thing \"active\", defaults to true.")
     private final Boolean active;
 
     @Builder
